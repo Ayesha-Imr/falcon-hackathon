9b3d997278a4d3579f84d29cbccb24390a64d8a4
Ayesha Imran <ayesha.i1505@gmail.com.com>
2024-07-30 13:43:54 +0500

    modified

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 app = Flask(__name__)
 CORS(app)
--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,8 @@ export default function Home() {
   const [greeting, setGreeting] = useState('');
 
   const handleGreeting = async () => {
-    try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/greet`, { name });
-      setGreeting(response.data.greeting);
-    } catch (error) {
-      console.error("Error fetching the greeting:", error);
-    }
+    const response = await axios.post('/greet', { name });
+    setGreeting(response.data.greeting);
   };
 
   return (
@@ -23,7 +19,7 @@ export default function Home() {
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className="border p-2 mb-4 w-full text-black"
+        className="border p-2 mb-4 w-full text-black" 
         placeholder="Enter your name"
       />
       <button
