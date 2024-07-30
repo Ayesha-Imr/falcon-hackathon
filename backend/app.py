d3b69bb3bbf4f28b43b4f4662fe3183a8a5d22b6
Ayesha Imran <ayesha.i1505@gmail.com.com>
2024-07-30 12:31:21 +0500

    modified

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app)
@@ -11,3 +16,7 @@ def greet():
     greeting = f"Welcome, {name}!"
     return jsonify({'greeting': greeting})
 
+env = os.getenv('FLASK_ENV', 'development')
+if env == 'development':
+    if __name__ == '__main__':
+        app.run(debug=True)
