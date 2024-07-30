3f0fd3ea90387f1343ed151775e3c1e82221996a
H-raheel <h.raheel622@gmail.com>
2024-07-30 21:06:46 +0500

    commented

--- backend/app.py
+++ backend/app.py
@@ -12,5 +12,5 @@ def greet():
     return jsonify({'greeting': greeting})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='127.0.0.1', port=5000)
